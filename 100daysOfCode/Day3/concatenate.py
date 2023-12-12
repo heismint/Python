15e71188ba6e61c953ba5af96cd1cb3d8cf9a02b
mint <mint@mints-MacBook-Air.local>
2023-12-12 01:25:15 +0100

    First few days

---
+++ 100daysOfCode/Day3/concatenate.py
@@ -0,0 +1,11 @@
+# All it really means is combine text (remember, text is called a string) and variables together into single sentences! 😲🤯
+
+typeOfFood = input("Name a type of food: ")
+typeOfPlants = input("Name a type of plant: ")
+typeOfCooking = input("Name a method of cooking: ")
+burnedFood = input("What word describes burned food?: ")
+householdItem = input("Name a household item: ")
+print()
+
+print("MENU")
+print(typeOfCooking, typeOfFood, "with", burnedFood, typeOfPlants, "on a bed of", householdItem)
